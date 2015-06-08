7658c0d1da37eb5b24c8e156540ed2236bd339b7
Andrea Nall <anall@andreanall.com>
2015-06-08 17:35:11 -0500

    Actually add install script

---
+++ java/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+apt-get install openjdk-7-jdk openjdk-7-jre
+cd java
+wget -O core.jar http://repo1.maven.org/maven2/com/google/zxing/core/3.2.0/core-3.2.0.jar
+wget -O javase.jar http://repo1.maven.org/maven2/com/google/zxing/javase/3.2.0/javase-3.2.0.jar
+javac -cp core.jar:javase.jar BarcodePDF417Decode.java
