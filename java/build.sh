d529c1ebe964dcc4563438a3e65e0a5f0e1dae9c
Andrea Nall <anall@andreanall.com>
2016-02-07 13:00:41 -0600

    attempt 3 at caching

--- java/build.sh
+++ java/build.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
+#!/bin/bash
+echo "Building local java files" &&
 mkdir -p lib &&
 javac -cp core.jar:javase.jar:lib -sourcepath src -d lib \
     src/BarcodePDF417Decode.java \
--- java/install.sh
+++ java/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 if [[ ! -e core.jar ]]; then wget -O core.jar http://repo1.maven.org/maven2/com/google/zxing/core/3.2.0/core-3.2.0.jar; fi &&
 if [[ ! -e javase.jar ]]; then wget -O javase.jar http://repo1.maven.org/maven2/com/google/zxing/javase/3.2.0/javase-3.2.0.jar; fi &&
 sh build.sh
