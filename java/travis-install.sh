ca48f5539b0a72735934b03b6dcf6dba714170dd
Andrea Nall <anall@andreanall.com>
2016-02-07 13:10:23 -0600

    attempt 6 at caching, sigh

--- java/travis-install.sh
+++ java/travis-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ -e $HOME/.cache/jar ]]; then cp $HOME/.cache/jar/*.jar java; fi &&
+if [[ -e $HOME/.cache/jar/core.jar ]]; then (cp $HOME/.cache/jar/*.jar .); fi &&
 if [[ -e core.jar ]]; then
     ./build.sh;
 else
