2789c478b468c2f03c90e80908a1168750c1f499
Andrea Nall <anall@andreanall.com>
2016-02-07 12:54:08 -0600

    Update for travis caching

--- java/install.sh
+++ java/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
-wget -O core.jar http://repo1.maven.org/maven2/com/google/zxing/core/3.2.0/core-3.2.0.jar &&
-wget -O javase.jar http://repo1.maven.org/maven2/com/google/zxing/javase/3.2.0/javase-3.2.0.jar &&
+#!/usr/bin/env bash
+if [[ ! -e core.jar ]]; then wget -O core.jar http://repo1.maven.org/maven2/com/google/zxing/core/3.2.0/core-3.2.0.jar; fi &&
+if [[ ! -e javase.jar ]]; then wget -O javase.jar http://repo1.maven.org/maven2/com/google/zxing/javase/3.2.0/javase-3.2.0.jar; fi &&
 sh build.sh
